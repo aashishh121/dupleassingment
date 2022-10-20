d276a8c10000b80fc683270b2cd1738878dcfff2
Ashish Kumar <98329891+aashishh121@users.noreply.github.com>
2022-10-20 22:30:06 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,5 +1,5 @@
 const mongoose  = require('mongoose');
-mongoose.connect("mongodb+srv://user123:MdppXZSAM0VN151R@cluster0.hzvo0.mongodb.net/book-store?retryWrites=true&w=majority");
+mongoose.connect("mongodb://localhost:27017/book-store");
 
 
 //"mongodb://localhost:27017/book-store"
